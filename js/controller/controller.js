583fd994a49dea2fc3a6e8bed35bacc81c15ca1c
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-09-23 20:02:14 +0900

    stacked changes

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,2 +1,46 @@
-const loginB = document.getElementByID('login_b');
-const signupB = document.getElementByID('signup_b');
+/**
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ * CONTROLLER for G9
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ */
+
+/**
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ * Elements Part
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ */
+
+const loginB = document.getElementById('login_button');
+const signupB = document.getElementById('signup_button');
+
+/**
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ * Arrow Functions Expression Part (not hoisted)
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ */
+
+const test = (method) => {
+  return `Now testing ${method} method...`;
+}
+
+const login = () => {
+  console.log(test('Log-In'));
+}
+
+const signup = () => {
+  console.log(test('Sign-Up'));
+}
+
+/**
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ * Event Attachment
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ */
+
+loginB.addEventListener('click', login);
+
+/**
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ * EOF
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ */
