aa0b889f2200b24f66ef3154167a0b77e00f0dc1
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-02 13:32:09 +0900

    comment on hydrate() usage

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// ReactDOM.render(element, container[, callback]); ...or,
+// ReactDOM.hydrate(element, container[, callback]);
+//   doesn't render, but only adds event handlers;
+//   required for SSR, except when the materials are full with markup in-Server
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
