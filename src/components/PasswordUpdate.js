b0f1fe466d7abc17cf5c195dd1ee7e7e657f5caf
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-04 15:44:41 +0900

    prevention of duplicate submits

--- src/components/PasswordUpdate.js
+++ src/components/PasswordUpdate.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 
 function PasswordUpdate() {
   const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(false);
 
   const handleChange = ({ target: { value } }) => setPassword(value);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
+    setDisabled(true);
     event.preventDefault();
+    await new Promise((r) => setTimeout(r, 1000));
     alert(`변경된 패스워드: ${password}`);
+    setDisabled(false);
   };
 
   return (
@@ -18,7 +22,9 @@ function PasswordUpdate() {
         value={password}
         onChange={handleChange}
       />
-      <button type="submit">비밀번호 변경</button>
+      <button type="submit" disabled={disabled}>
+        비밀번호 변경
+      </button>
     </form>
   );
 }
