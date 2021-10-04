0843fe1ef0b486cc84cfb174d0a3a4801efc03e5
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-04 18:19:47 +0900

    added Routes and stored multiple layouts in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Nav from './components/Nav';
@@ -6,13 +9,21 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  return (
+  const myDesign = (
     <div className="App">
       <Header></Header>
       <Main></Main>
       <Nav></Nav>
       <Footer></Footer>
     </div>
+  );
+  const leedr0730 = (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+  return (
+    myDesign
   );
 }
 
---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Switch, Redirect, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Layout from "./components/Layout";
+function Routes() {
+  let isAuthorized = sessionStorage.getItem("isAuthorized");
+
+  return (
+    <div>
+      {!isAuthorized ? <Redirect to="/login" /> : <Redirect to="/" />}
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/">
+          <Layout />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+export default Routes;
