67ef2d8e6472d4d1772ea69ff53c184bf99daac4
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-16 14:59:38 +0900

    moved App to components + others

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 
-import Header from './components/Header';
-import Main from './components/Main';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Dispatcher from './Dispatcher/Dispatcher';
-import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Nav from './Nav';
+import Footer from './Footer';
+import Dispatcher from '../Dispatcher/Dispatcher';
+import '../css/App.css';
 
 // 잘 생각해보자, 앱의 기본 레이아웃을 어찌 해야할 지...
 
