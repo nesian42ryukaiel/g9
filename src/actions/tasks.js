2108649a9ff12df802bd18bbb6d4245e2f1d506a
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-16 14:56:04 +0900

    added tasks as js

---
+++ src/actions/tasks.js
@@ -0,0 +1,12 @@
+export const signupTask = (task) => ({
+  type: 'SIGNUP_TASK',
+  payload: {
+    task
+  }
+});
+export const loginTask = (task) => ({
+  type: 'LOGIN_TASK',
+  payload: {
+    task
+  }
+});
