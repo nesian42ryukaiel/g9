fb51b5efd3d4948fb8f4ef294ec810f70601b01b
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-16 14:33:22 +0900

    eliminated Routes for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
-import Routes from "./Routes";
 
-import Header from './Components/Header';
-import Main from './Components/Main';
-import Nav from './Components/Nav';
-import Footer from './Components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 import Dispatcher from './Dispatcher/Dispatcher';
 import './App.css';
 
@@ -20,11 +19,6 @@ function App() {
       <Footer></Footer>
       <Dispatcher />
     </div>
-  );
-  const leedr0730 = (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
   );
   return (
     myDesign
