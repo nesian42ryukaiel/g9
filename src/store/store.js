2903d6810938e6bd10658312aad1afaa0fb2b347
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-19 22:37:34 +0900

    create store with initial state?

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers/rootReducer'
+import { initialState } from '../state/state';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, initialState)
 
 export default store;
