90183ee56e09d01e9547e70d8ac527aa30f14237
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-19 22:17:44 +0900

    login and signup won't need payload, probably...

--- src/actions/membershipActions.js
+++ src/actions/membershipActions.js
@@ -3,13 +3,7 @@ export const LOGIN_TASK = 'LOGIN_TASK';
 
 export const signup = (task) => ({
   type: 'SIGNUP_TASK',
-  payload: {
-    task
-  }
 });
 export const login = (task) => ({
   type: 'LOGIN_TASK',
-  payload: {
-    task
-  }
 });
