a675d13a4eba2315d9599867e33e831d09c43371
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-21 18:20:52 +0900

    will be divided to each module(s)

--- src/state/state.js
+++ src/state/state.js
@@ -2,6 +2,8 @@
  * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
  * The initial state, especially members and articles,
  * will eventually be imported from the server's JSON storage, I think.
+ * 
+ * ... deprecated to be divided to each Duck module!
  * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
  */
 
