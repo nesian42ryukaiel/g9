ee656c76cccfe2b8621e997f0d0d7ed4c65de72a
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-21 17:59:31 +0900

    finished my first true Ducks patterned module

---
+++ src/modules/membership.js
@@ -0,0 +1,56 @@
+/**
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ * Membership: my first true Ducks pattern module
+ * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
+ */
+
+import { initialState } from "../state/state";
+
+const LOGIN = 'membership/LOGIN';
+const SIGNUP = 'membership/SIGNUP';
+
+export const login = () => ({
+  type: 'LOGIN_TASK',
+});
+export const signup = () => ({
+  type: 'SIGNUP_TASK',
+});
+
+export default function membershipReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN:
+      if (
+        state.id === state.members[0].mid &&
+        state.password === state.members[0].mpw
+      ) {
+        console.log(`Welcome back, ${state.id}!`);
+        return {
+          ...state,
+          login: true,
+          page: 'indexPage'
+        }
+      } else {
+        console.log('Login Failed!');
+        return state;
+      }
+    case SIGNUP:
+      if (
+        state.id === state.members[0].mid
+      ) {
+        console.log('Member exists!');
+        return state;
+      } else {
+        console.log(`Welcome to G9, ${state.id}!`);
+        const newmem = {
+          mid: state.id,
+          mpw: state.password
+        };
+        return {
+          ...state,
+          members: state.members.concat([newmem])
+        };
+      }
+    default:
+      return state;
+  }
+}
