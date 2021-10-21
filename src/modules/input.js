5b710e18cade8395b802d9b7eeb6dca529aa621d
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-21 18:07:01 +0900

    built second Ducks module

---
+++ src/modules/input.js
@@ -0,0 +1,34 @@
+import { initialState } from "../state/state";
+
+export const INPUT_ID = 'input/ID';
+export const INPUT_PASS = 'input/PASS';
+
+export const inputID = (task) => ({
+  type: INPUT_ID,
+  payload: {
+    task
+  }
+});
+export const inputPass = (task) => ({
+  type: INPUT_PASS,
+  payload: {
+    task
+  }
+});
+
+export default function inputReducer(state = initialState, action) {
+  switch (action.type) {
+    case INPUT_ID:
+      return {
+        ...state,
+        id: action.payload.task
+      };
+    case INPUT_PASS:
+      return {
+      ...state,
+      password: action.payload.task
+      };
+    default:
+      return state;
+  }
+}
