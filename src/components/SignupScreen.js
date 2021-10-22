70154502d3fed59fe7e8298d072490eb1e7f0bfa
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-22 17:42:08 +0900

    MemberInput merged into SignupScreen

--- src/components/SignupScreen.js
+++ src/components/SignupScreen.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import LoginButton from './LoginButton';
-import MemberInput from './MemberInput';
+// import MemberInput from './MemberInput';
 import SignupButton from './SignupButton';
 
 export default function SignupScreen() {
   return (
     <main>
-      <div>
-        <MemberInput />
+      <div className='signup__memberInput'>
+        <div>
+          <input
+          type='text' className='user_id--input'
+          placeholder='ID' // onChange={(e) => store.dispatch(inputID(e.target.value))}
+          />
+        </div>
+        <div>
+          <input
+          type='text' className='password--input'
+          placeholder='Password' // onChange={(e) => store.dispatch(inputPass(e.target.value))}
+          />
+        </div>
       </div>
-      <div>
+      <div className='memberInput'>
         <span>
           <LoginButton />
           <SignupButton />
