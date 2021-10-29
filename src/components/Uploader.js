c6e0aa285a41fec28208d034564f447caacd1a46
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-29 20:16:58 +0900

    fattening up uploader

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 function Uploader() {
+  const author = 'u/author';
+  // fetch a function through reducer to add new article when clicked
   return (
     <div className='Uploader centralize corefunc'>
-      <input type="file" id="input-article-image" class="image_inputType_file" accept="img/*,.mp4" />
-      <button class="input-article-new">Upload</button>
+      <form>
+        <div>
+          <label for='file'>Choose image or video to upload</label><br />
+          <input type="file" id="input-article-image" class="image_inputType_file" accept="img/*,.mp4" /><br />
+          <input type='text' id='input-article-title' placeholder='Title' /><br />
+          <input type='text' id='input-article-text' placeholder='addttional text' />
+        </div>
+        <div>
+        <button class="input-article-new">Upload</button>
+        </div>
+      </form>
     </div>
   );
 }
