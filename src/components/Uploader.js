a16df1706e89de0816d4cc88d927590c7bddb9b0
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-10-30 17:40:15 +0900

    fixed css

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -8,13 +8,13 @@ function Uploader() {
       <form>
         <div>
           <label for='file'>Choose image or video to upload</label><br />
-          <input type="file" id="input-article-image" class="image_inputType_file"
+          <input type="file" id="input-article-image input--file" class="image_inputType_file"
           accept=".gif, .jpeg, .jpg, .png, .mp4" /><br />
           <input type='text' id='input-article-title' placeholder='Title (required)' /><br />
           <input type='text' id='input-article-text' placeholder='optional text' />
         </div>
         <div>
-        <button class="input-article-new">Upload</button>
+          <input type='submit' className='input-article-new input--button' value='Upload' />
         </div>
       </form>
     </div>
