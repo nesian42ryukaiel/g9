35480a8634776460e72220575c02c10cae9fa25a
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-11-02 17:57:37 +0900

    implemented submission handlers and conditional displays

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -4,8 +4,16 @@ function Uploader() {
   const author = 'u/author';
   // fetch a function through reducer to add new article when clicked
   // tips from https://www.pluralsight.com/guides/uploading-files-with-reactjs
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(); // should contain info on currently picked file
+  const [isFilePicked, setIsFilePicked] = useState(false); // should determine if a file has been picked or not
+  const changeHandler = (e) => {
+    setSelectedFile(e.target.files[0]);
+    setIsFilePicked(true);
+    console.log('Accessed changeHandler('+e.target.files[0].name+')!');
+  };
+  const handleSubmission = () => {
+    console.log('Submitted!');
+  };
   return (
     <div className='Uploader centralize corefunc'>
       <form>
@@ -13,18 +21,36 @@ function Uploader() {
           <img id='ul--output' width='256' />
         </div>
         <div>
-          <label htmlFor='file'>Choose image or video to upload</label><br />
-          <input type="file" id="input-article-image input--file" className="image_inputType_file"
+          <input type="file" id="input-article-image input--file"
+          className="image_inputType_file"
           accept=".gif, .jpeg, .jpg, .png, .mp4"
           onChange={(e) => {
             let image = document.getElementById('ul--output');
             image.src = URL.createObjectURL(e.target.files[0]);
+            changeHandler(e);
           }} /><br />
-          <input type='text' id='input-article-title' placeholder='Title (required)' /><br />
-          <input type='text' id='input-article-text' placeholder='optional text' />
+          {/* <label htmlFor='file'>Choose image or video to upload</label><br /> */}
+          {isFilePicked ? (
+            <div>
+              <p>Filename: {selectedFile.name}</p>
+              <p>Filetype: {selectedFile.type}</p>
+              <p>Size in bytes: {selectedFile.size}</p>
+              <p>
+                lastModifiedDate:{' '}
+                {selectedFile.lastModifiedDate.toLocaleDateString()}
+              </p>
+            </div>
+          ) : (
+            <p>Choose image or video to upload</p>
+          ) }
+          <input type='text' id='input-article-title'
+          placeholder='Title (required)' /><br />
+          <input type='text' id='input-article-text'
+          placeholder='optional text' />
         </div>
         <div>
-          <input type='submit' className='input-article-new input--button' value='Upload' />
+          <input type='button' className='input-article-new input--button'
+          value='Upload' onClick={handleSubmission} />
         </div>
       </form>
     </div>
