4bb4002f15c29d7a661999807111e9c1f20671ca
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-11-03 21:02:38 +0900

    removed unnecessary imports

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import SignupButton from './SignupButton';
 
 function Nav() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux'
 import rootReducer from './modules/rootReducer';
 import AppContainer from './containers/AppContainer';
-import App from './components/App';
 
 import './css/index.css';
 
