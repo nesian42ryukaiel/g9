0fece6d29e645526f0f4736252ba103659b2bd80
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-11-03 19:18:05 +0900

    the img tag's alt property is web standard

--- src/components/Article.js
+++ src/components/Article.js
@@ -7,7 +7,9 @@ function Article({title, image, text}) {
     <article className='Article'>
       <div className="article__title">{title}</div>
       <div className="article__image">
-        <img src={image} />
+        <img src={image}
+        alt='the main dish'
+        />
       </div>
       <div className="article__text">{text}</div>
     </article>
