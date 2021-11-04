facf76e8a1e5273007e574522a91928feb920ac4
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-11-04 17:58:59 +0900

    article order reversed too

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Article from './Article';
 
 function Main({articles}) {
+  let revart = articles.slice().reverse();
   return (
     <main className='corefunc Main'>
-      {articles.map((object, i) => <Article key={i} title={object.title} image={object.image} text={object.text} />)}
+      {revart.map((object, i) => <Article key={i} title={object.title} image={object.image} text={object.text} />)}
     </main>
   );
 }
