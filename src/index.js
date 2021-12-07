be8c28b97560117429afc7d79b37367917bf8c8f
nesian42ryukaiel <nesianryukaiel@gmail.com>
2021-12-07 17:35:41 +0900

    wipe comment

--- src/index.js
+++ src/index.js
@@ -12,39 +12,6 @@ import serverLink from './pseudoLinks/links'; // test purposes
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-/**
- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
- * json-server test area begin
- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
- */
-
-// const getData = (link) => {
-//   fetch((link)
-//   , {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json'
-//     }
-//   })
-//   .then(function(response) {
-//     console.log(response);
-//     return response.json();
-//   })
-//   .then(function(myJson) {
-//     console.log(myJson);
-//     console.log('Final text data below:');
-//     console.log(myJson.text);
-//   })
-// }
-
-// getData(serverLink);
-
-/**
-* -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
-* json-server test area end
-* -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- * -=-|-=- *
-*/
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
