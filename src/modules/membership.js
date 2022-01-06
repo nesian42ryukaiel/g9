7a660b51a4313233f01c92b8c1cb213bad2896a2
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-06 19:28:08 +0900

    seems to work for now...

--- src/modules/membership.js
+++ src/modules/membership.js
@@ -125,7 +125,7 @@ export default function membership(state = initialState, action) {
               ...state,
               members: {
                 ...state.members,
-                newkey: newmem, // fixing here
+                [newkey]: newmem, // fixing here
               },
             };
           }
