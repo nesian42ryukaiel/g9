023d699d3290425694628b04c6cecd43629cd886
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-07 17:39:27 +0900

    cleanup unnecessary imports

--- src/containers/UploaderContainer.js
+++ src/containers/UploaderContainer.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Uploader from "../components/Uploader";
+// import Uploader from "../components/Uploader";
 import UploadForm from "../components/UploadForm";
-import { movePage } from "../modules/pages";
+// import { movePage } from "../modules/pages";
 
 function UploadContainer() {
-  const { id } = useSelector(state => ({
-    id: state.membership.id
+  const { id } = useSelector((state) => ({
+    id: state.membership.id,
   }));
   const dispatch = useDispatch();
   // function to concat new article
