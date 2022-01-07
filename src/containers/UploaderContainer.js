0d5b532a31c0bf90bc1540732b99c3acb0b63cf5
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-07 17:40:28 +0900

    disabled useSelector and useDispatch from UploaderContainer

--- src/containers/UploaderContainer.js
+++ src/containers/UploaderContainer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 // import Uploader from "../components/Uploader";
 import UploadForm from "../components/UploadForm";
 // import { movePage } from "../modules/pages";
 
 function UploadContainer() {
-  const { id } = useSelector((state) => ({
-    id: state.membership.id,
-  }));
-  const dispatch = useDispatch();
+  // const { id } = useSelector((state) => ({
+  //   id: state.membership.id,
+  // }));
+  // const dispatch = useDispatch();
   // function to concat new article
   return (
     // <Uploader id={id}/>
