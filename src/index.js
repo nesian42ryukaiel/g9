aa6f1e450226eacc2271f78aba080cff9ff91930
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-07 17:44:11 +0900

    merged configureStoreAsync into index-js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux"
+import { createStore } from "redux";
 import rootReducer from "./modules/rootReducer";
 import AppContainer from "./containers/AppContainer";
-import configureStoreAsync from "./modules/configureStoreAsync";
+// import configureStoreAsync from "./modules/configureStoreAsync";
+import { pServerLink, artdata } from "./pseudoLinks/links";
+import axios from "axios";
 import "./css/index.css";
 
-// import serverLink from "./pseudoLinks/links"; // test purposes
+const initialStoreState = {
+  editor: {
+    uploader: {
+      ufile: "",
+      utitle: "",
+      utext: "",
+    },
+    editor: {
+      efile: "",
+      etitle: "",
+      etext: "",
+    },
+  },
+  membership: {
+    loggedin: false,
+    id: "",
+    password: "",
+    members: {},
+  },
+  pages: {
+    currentPage: "index",
+    articles: [],
+  },
+};
 
-//const store = createStore(rootReducer); // should I move this to configureStoreAsync?
-//console.log(store.getState());
+function configureStoreAsync() {
+  return new Promise((resolve) => {
+    let initialState = initialStoreState; //default initial store state
+    try {
+      // do some async stuff here to manipulate initial state
+      // ...like read from local disk etc.
+      // This is again wrapped in its own Promises.
+      axios
+        .get(pServerLink + "/" + artdata)
+        .then(function (response) {
+          // console.log("RSP: " + JSON.stringify(response));
+          // console.log("RSD: " + JSON.stringify(response.data));
+          initialState.pages.articles = JSON.parse(
+            JSON.stringify(response.data)
+          );
+          // console.log("new initialState: " + initialState)
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .finally(function () {
+          // always executed
+          const store = createStore(rootReducer, initialState);
+          // console.log(store.getState());
+          resolve(store);
+        });
+    } catch (error) {
+      // To do .... log error!
+      const store = createStore(rootReducer, initialState);
+      console.log(store.getState());
+      resolve(store);
+    }
+  });
+}
 
-configureStoreAsync().then(result => {
+configureStoreAsync().then((result) => {
   const store = result;
   console.log(store.getState());
   return ReactDOM.render(
