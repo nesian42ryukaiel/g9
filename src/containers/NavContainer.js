943c4b469370da32c2a05659c0530b88f60bfa6f
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-07 17:32:00 +0900

    removed useDispatch from NavContainer

--- src/containers/NavContainer.js
+++ src/containers/NavContainer.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import Nav from "../components/Nav";
 // import { movePage } from "../modules/pages";
 
 function NavContainer() {
-  const { articles } = useSelector(state => ({
-    articles: state.pages.articles
+  const { articles } = useSelector((state) => ({
+    articles: state.pages.articles,
   }));
   /**
    * functions go here
    */
-  return (
-    <Nav
-    articles={articles}
-    />
-  );
+  return <Nav articles={articles} />;
 }
 
 export default NavContainer;
