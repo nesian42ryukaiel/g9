c388ccceea254ca45b9ea19986539a28a9c0cdb1
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-07 17:27:05 +0900

    added maintenance data for hamburger emoji

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,22 @@ import React from "react";
 function Header({ loggedin, id, moveFunc }) {
   const onClickMoveToIndexPage = () => {
     moveFunc("index");
-  }
+  };
   const onClickMoveToLoginPage = () => {
     moveFunc("login");
-  }
+  };
   const onClickMoveToSignupPage = () => {
     moveFunc("signup");
-  }
+  };
   return (
     <>
       <span className="header__left">
-        <span className="hamburger">🍔</span>
-        <span className="header--logo"
-        onClick={onClickMoveToIndexPage}>G9</span>
+        <span role="img" aria-label="hamburger" className="hamburger">
+          🍔
+        </span>
+        <span className="header--logo" onClick={onClickMoveToIndexPage}>
+          G9
+        </span>
       </span>
       <span className="header__middle">
         {/* <span>Options</span> */}
@@ -24,14 +27,21 @@ function Header({ loggedin, id, moveFunc }) {
         </span>
       </span>
       <span className="header__right">
-        {loggedin 
-        ? <>Welcome, {id}!</>
-        : <>
-          <button className="login--button"
-          onClick={onClickMoveToLoginPage}>Login</button>
-          <button className="signup--button"
-          onClick={onClickMoveToSignupPage}>Sign-up</button>
-        </>}
+        {loggedin ? (
+          <>Welcome, {id}!</>
+        ) : (
+          <>
+            <button className="login--button" onClick={onClickMoveToLoginPage}>
+              Login
+            </button>
+            <button
+              className="signup--button"
+              onClick={onClickMoveToSignupPage}
+            >
+              Sign-up
+            </button>
+          </>
+        )}
       </span>
     </>
   );
