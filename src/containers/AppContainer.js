0290fadf2406590be78d173d19576b9d23d01547
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-07 17:32:38 +0900

    removed useState and useDispatch from AppContainer

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,16 +1,12 @@
-import React, {useState} from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import App from "../components/App";
 
 function AppContainer() {
-  const { currentPage } = useSelector(state => ({
+  const { currentPage } = useSelector((state) => ({
     currentPage: state.pages.currentPage,
   }));
-  return (
-    <App
-    page={currentPage}
-    />
-  );
+  return <App page={currentPage} />;
 }
 
 // 대충 원하는 component 수 만큼 각각 connect로 연결하기
