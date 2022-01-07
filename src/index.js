eaf5cac986598a29fdbbb78245871f58f5bda59e
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-07 17:58:27 +0900

    removed unnecessary comments

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./modules/rootReducer";
 import AppContainer from "./containers/AppContainer";
-// import configureStoreAsync from "./modules/configureStoreAsync";
 import { pServerLink, artdata } from "./pseudoLinks/links";
 import axios from "axios";
 import "./css/index.css";
@@ -36,32 +35,23 @@ const initialStoreState = {
 
 function configureStoreAsync() {
   return new Promise((resolve) => {
-    let initialState = initialStoreState; //default initial store state
+    let initialState = initialStoreState;
     try {
-      // do some async stuff here to manipulate initial state
-      // ...like read from local disk etc.
-      // This is again wrapped in its own Promises.
       axios
         .get(pServerLink + "/" + artdata)
         .then(function (response) {
-          // console.log("RSP: " + JSON.stringify(response));
-          // console.log("RSD: " + JSON.stringify(response.data));
           initialState.pages.articles = JSON.parse(
             JSON.stringify(response.data)
           );
-          // console.log("new initialState: " + initialState)
         })
         .catch(function (error) {
           console.log(error);
         })
         .finally(function () {
-          // always executed
           const store = createStore(rootReducer, initialState);
-          // console.log(store.getState());
           resolve(store);
         });
     } catch (error) {
-      // To do .... log error!
       const store = createStore(rootReducer, initialState);
       console.log(store.getState());
       resolve(store);
