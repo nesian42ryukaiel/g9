97934f602dc96575a9d5c192cce66bb7c3358edd
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-01-12 19:26:01 +0900

    header display change issues

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -4,18 +4,13 @@ import Header from "../components/Header";
 import { movePage } from "../modules/pages";
 
 function HeaderContainer() {
-  const { loggedin, id  } = useSelector(state => ({
-    loggedin: state.membership.loggedin,
-    id: state.membership.id,
+  const { loggedin, id } = useSelector((state) => ({
+    loggedin: state.membership.mlogin,
+    id: state.membership.mid,
   }));
   const dispatch = useDispatch();
   const onMovePage = (page) => dispatch(movePage(page));
-  return (
-    <Header
-    loggedin={loggedin} id={id}
-    moveFunc={onMovePage}
-    />
-  );
+  return <Header loggedin={loggedin} id={id} moveFunc={onMovePage} />;
 }
 
 export default HeaderContainer;
