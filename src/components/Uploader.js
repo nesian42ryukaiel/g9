b3a30ed1dc9c9b8e23e1d2de0ee1025fc75e9ac1
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-02-10 20:11:32 +0900

    server side done too

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import serverLink from "../pseudoLinks/links"; // probably will be omitted
 import { pServerLink } from "../pseudoLinks/links";
 import Base64 from "../modules/Base64";
 
