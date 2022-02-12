5fd2e582659330fcb75ac929e742c08987f2fdfd
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-02-12 16:51:42 +0900

    cut redundancy

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { pServerLink } from "../pseudoLinks/links";
 import Base64 from "../modules/Base64";
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import App from "../components/App";
+import { movePage } from "../modules/pages";
 
 function AppContainer() {
   const { currentPage } = useSelector((state) => ({
     currentPage: state.pages.currentPage,
   }));
-  return <App page={currentPage} />;
+  const dispatch = useDispatch();
+  const move = (page) => dispatch(movePage(page));
+  return <App page={currentPage} move={move} />;
 }
 
 // 대충 원하는 component 수 만큼 각각 connect로 연결하기
