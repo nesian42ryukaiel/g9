b9db7bca51913a652b823a9342f36f5b923f64b7
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-02-15 19:13:06 +0900

    added outline to each Article on CSS

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -2,11 +2,13 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Header from "../components/Header";
 import { movePage } from "../modules/pages";
+import { mediaToggle } from "../modules/media";
 
 function HeaderContainer() {
-  const { loggedin, id } = useSelector((state) => ({
+  const { loggedin, id, isdarkmode } = useSelector((state) => ({
     loggedin: state.membership.mlogin,
     id: state.membership.mid,
+    isdarkmode: state.media.darkmode,
   }));
   const dispatch = useDispatch();
   const onMovePage = (page) => dispatch(movePage(page));
