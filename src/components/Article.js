4032f1938098a15bcebce3c73faed9c9b25b3605
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-02-25 19:10:56 +0900

    repositioned by padding as close to initial plan as possible

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,13 +4,13 @@ function Article({ title, image, text, location }) {
   // const pic = require(image);
   // so far no usage for key
   return (
-    <article className={`Article ${location}`}>
+    <article className={`Article`}>
       {location === "Main__articles" || location === "main__articles" ? (
         <div className="Article__title">{title}</div>
       ) : (
         <></>
       )}
-      <div className="Article__imageBox">
+      <div className={`Article__imageBox ${location}--padding`}>
         <img
           src={image}
           className={`Article__image Article__image--blur ${location}`}
