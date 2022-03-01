4746a9222701b0509b46ef193e2e9e4706751dde
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-03-01 17:51:12 +0900

    was this all it took to succeed???

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 
 import HeaderContainer from "../containers/HeaderContainer";
 import NavContainer from "../containers/NavContainer";
@@ -45,7 +46,9 @@ function App({ page, move }) {
         {pagelist[page]}
         <NavContainer />
         <FooterContainer />
-        <BetaContainer />
+        <MediaQuery minWidth={640}>
+          <BetaContainer />
+        </MediaQuery>
         {/* <div className="Beta__veil centralize">VEIL</div> */}
       </div>
     </>
