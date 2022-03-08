fc5675453ef239353b16df546b02cd48a6a2634a
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-03-08 21:01:14 +0900

    first trial of using local storage

---
+++ src/modules/useLocalStorageState.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+export default function useLocalStorageState(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const persistedValue = localStorage.getItem(key);
+    return persistedValue !== null ? persistedValue : initialValue;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+  return [value, setValue];
+}
