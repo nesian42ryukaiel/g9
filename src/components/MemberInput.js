6523bafe84f1939cc4c08f4aae0f32cc963705d8
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-03-16 17:57:15 +0900

    comment cleanup

--- src/components/MemberInput.js
+++ src/components/MemberInput.js
@@ -1,20 +1,13 @@
 import React from "react";
-// import { inputID, inputPass } from "../actions/inputActions";
 
 export default function MemberInput() {
   return (
     <>
       <div>
-        <input
-        type="text" className="user_id--input"
-        placeholder="ID" // onChange={(e) => store.dispatch(inputID(e.target.value))}
-        />
+        <input type="text" className="user_id--input" placeholder="ID" />
       </div>
       <div>
-        <input
-        type="text" className="password--input"
-        placeholder="Password" // onChange={(e) => store.dispatch(inputPass(e.target.value))}
-        />
+        <input type="text" className="password--input" placeholder="Password" />
       </div>
     </>
   );
