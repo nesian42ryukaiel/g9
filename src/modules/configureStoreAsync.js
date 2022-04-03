ee7844a93063f00761b3394232800e2be106cc6f
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-04-03 09:01:36 +0900

    comment cleanup

--- src/modules/configureStoreAsync.js
+++ src/modules/configureStoreAsync.js
@@ -1,48 +1,50 @@
-import { createStore } from "redux"
+import { createStore } from "redux";
 import rootReducer from "../modules/rootReducer";
 import { pServerLink, artdata } from "../pseudoLinks/links";
 import axios from "axios";
 
 const initialStoreState = {
-  "editor": {
-    "uploader": {
-      "ufile": "",
-      "utitle": "",
-      "utext": ""
+  editor: {
+    uploader: {
+      ufile: "",
+      utitle: "",
+      utext: "",
     },
-    "editor": {
-      "efile": "",
-      "etitle": "",
-      "etext": ""
-    }
+    editor: {
+      efile: "",
+      etitle: "",
+      etext: "",
+    },
   },
-  "membership": {
-    "loggedin": false,
-    "id": "",
-    "password": "",
-    "members": {}
+  membership: {
+    loggedin: false,
+    id: "",
+    password: "",
+    members: {},
   },
-  "pages": {
-    "currentPage": "index",
-    "articles": []
-  }
+  pages: {
+    currentPage: "index",
+    articles: [],
+  },
 };
 
 // So apparently axios already returns a JS Promise, huh...
 
 export default function configureStoreAsync() {
   return new Promise((resolve) => {
-    let initialState = initialStoreState;//default initial store state
+    let initialState = initialStoreState;
     try {
       // do some async stuff here to manipulate initial state
-      // ...like read from local disk etc. 
+      // ...like read from local disk etc.
       // This is again wrapped in its own Promises.
       axios
         .get(pServerLink + "/" + artdata)
         .then(function (response) {
           // console.log("RSP: " + JSON.stringify(response));
           // console.log("RSD: " + JSON.stringify(response.data));
-          initialState.pages.articles = JSON.parse(JSON.stringify(response.data));
+          initialState.pages.articles = JSON.parse(
+            JSON.stringify(response.data)
+          );
           // console.log("new initialState: " + initialState)
         })
         .catch(function (error) {
@@ -61,4 +63,4 @@ export default function configureStoreAsync() {
       resolve(store);
     }
   });
-};
+}
